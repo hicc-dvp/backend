f8176dea43cd781a1f9590050eb151721ba3f789
jiwonkim <andrewkimswe@gmail.com>
2025-02-21 17:46:22 +0900

    Initial commit

---
+++ src/main/java/hicc/club_fair_2025/ClubFair2025Application.java
@@ -0,0 +1,13 @@
+package hicc.club_fair_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubFair2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubFair2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/hicc/club_fair_2025/ClubFair2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package hicc.club_fair_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubFair2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
