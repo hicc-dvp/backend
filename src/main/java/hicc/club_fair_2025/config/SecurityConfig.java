b487525f9e49ad554c901062dbc4fc3c68ef4581
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2025-03-05 16:23:11 +0900

    hotfix : 문법 수정

--- src/main/java/hicc/club_fair_2025/config/SecurityConfig.java
+++ src/main/java/hicc/club_fair_2025/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 	    CorsConfiguration configuration = new CorsConfiguration();
-	    configuration.setAllowedOrigins(List.of("https://hicc.space", "https://api.hicc.space" "http://localhost:5173"));
+	    configuration.setAllowedOrigins(List.of("https://hicc.space", "https://api.hicc.space", "http://localhost:5173"));
 	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 	    configuration.setAllowedHeaders(List.of("*"));
 	    configuration.setAllowCredentials(true);
